3cb875e9a5d70e38a0b8769c9ba1c447404633f9
Isuru Walpola <iwalpola@gmail.com>
2021-12-14 22:38:42 +0530

    add current position button

--- lib/ui/map.dart
+++ lib/ui/map.dart
@@ -4,6 +4,7 @@ import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controller = Completer();
   CustomInfoWindowController infoWindowController =
       CustomInfoWindowController();
   static const CameraPosition _kGooglePlex = CameraPosition(
@@ -52,12 +53,12 @@ class _MapScreenState extends State<MapScreen> {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Icon(
+                            const Icon(
                               Icons.monetization_on,
                               color: Colors.white,
                               size: 30,
                             ),
-                            SizedBox(
+                            const SizedBox(
                               width: 8.0,
                             ),
                             Expanded(
@@ -80,17 +81,17 @@ class _MapScreenState extends State<MapScreen> {
                   ),
                 ],
               ),
-              LatLng(37.42796133580664, -122.085749655962),
+              const LatLng(37.42796133580664, -122.085749655962),
             );
           },
-          markerId: MarkerId("1"),
-          position: LatLng(37.42796133580664, -122.085749655962))
+          markerId: const MarkerId("1"),
+          position: const LatLng(37.42796133580664, -122.085749655962))
     ];
 
     return Scaffold(
       appBar: AppBar(
         systemOverlayStyle: SystemUiOverlayStyle.light,
-        title: Text("BasePay"),
+        title: const Text("BasePay"),
       ),
       body: Stack(
         children: [
@@ -120,11 +121,84 @@ class _MapScreenState extends State<MapScreen> {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Add wage',
-        child: const Icon(Icons.add),
+      floatingActionButton: Stack(
+        children: [
+          Positioned(
+            right: 0,
+            bottom: 70,
+            child: FloatingActionButton(
+              heroTag: 'addstub',
+              onPressed: () {/* Do something */},
+              child: const Icon(
+                Icons.add,
+                size: 40,
+              ),
+              shape: const CircleBorder(),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: FloatingActionButton(
+              onPressed: () async {
+                Position position = await _determinePosition();
+                infoWindowController.googleMapController?.animateCamera(
+                    CameraUpdate.newCameraPosition(CameraPosition(
+                        target: LatLng(position.latitude, position.longitude),
+                        zoom: 15.0)));
+              },
+              child: const Icon(
+                Icons.gps_fixed,
+                size: 20,
+              ),
+              shape: const CircleBorder(),
+            ),
+          ),
+          // Add more floating buttons if you want
+          // There is no limit
+        ],
       ),
     );
+  }
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
